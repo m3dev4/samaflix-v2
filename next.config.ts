5e73188d7305060c907dc8e6d0f73a98e7030816
m3dev4 <m3dev4@gmail.com>
2025-03-23 04:56:52 +0000

    Modification de la configuration dans next.config.ts pour changer le type de sortie en 'export' et suppression de la minification SWC.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  swcMinify: false,
+  output: 'export',
   images: {
     unoptimized: true,
     domains: [
