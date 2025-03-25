f208c05dc8b7e011569041d2f13304d4d66f5554
m3dev4 <m3dev4@gmail.com>
2025-03-25 06:45:13 +0000

    Remove standalone output configuration and experimental features from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -11,32 +11,6 @@ const nextConfig: NextConfig = {
       "www.cpasmieux.ad"
     ],
   },
-  output: 'standalone',
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-      allowedOrigins: ['*']
-    },
-    optimizeCss: true,
-  },
-  webpack: (config, { isServer, dev }) => {
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-      splitChunks: {
-        chunks: 'all',
-        maxSize: 20000000, // 20MB max par chunk
-      }
-    }
-    if (isServer) {
-      config.output = {
-        ...config.output,
-        filename: '[name].js',
-        chunkFilename: '[name].[chunkhash].js'
-      };
-    }
-    return config;
-  }
 };
 
 export default nextConfig;
